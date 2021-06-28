5fcc3b079b0ed6479d2d68e8c453b1708cd71fd7
Ifen Faridian <ifeniprun16@gmail.com>
2021-06-28 18:17:01 +0700

    add laravel ui auth

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,10 @@ Route::prefix('admin')->group(function(){
         // Portfolio Routes
         Route::resource('portfolio', PortfolioController::class);
     });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
