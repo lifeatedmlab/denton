f5142e856d1902557e78ed8a8e61f5d213b8d807
Ifen Faridian <ifeniprun16@gmail.com>
2021-10-12 00:07:56 +0700

    crud position

---
+++ app/Http/Controllers/PositionController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Position;
+use Illuminate\Http\Request;
+
+class PositionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $positions=Position::all();
+        return view('admin.position.index', compact('positions'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.position.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Position::create([
+            'position' => $request->position,
+        ]);
+
+        if ($request->submit == 'add') {
+            return redirect()->route('position.create')
+                ->with('success', 'Position has been successfully created');
+        }
+        return redirect()->route('position.index')
+            ->with('success', 'Position has been successfully created');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Position  $position
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Position $position)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Position  $position
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Position $position)
+    {
+        return view('admin.position.edit', compact('position'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Position  $position
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Position $position)
+    {
+        $position = Position::findOrFail($position->id);
+        $position->update([
+            'position' => $request->position,
+        ]);
+        return redirect()->route('position.index')
+            ->with('success', 'Position has been successfully edited');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Position  $position
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Position $position)
+    {
+        $position->delete();
+        return redirect()->route('position.index')
+            ->with('success', 'Position deleted successfully');
+    }
+}
